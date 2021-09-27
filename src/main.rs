0449dd46951e4fde7dd2761a6f430656e72cb81d
mat <github@matdoes.dev>
2021-09-26 22:38:09 -0500

    switch to kagamijxl

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,23 @@
 extern crate rocket;
 use image::io::Reader as ImageReader;
 use rocket_dyn_templates::Template;
-use std::collections::HashMap;
+use std::{collections::HashMap, fs};
 
 extern crate rocket_multipart_form_data;
 
 use rocket::http::ContentType;
 use rocket::Data;
+use rocket::State;
 
-use jpegxl_rs::image::*;
+use kagamijxl;
 use rocket_multipart_form_data::{
     mime, MultipartFormData, MultipartFormDataField, MultipartFormDataOptions,
 };
+
+use dotenv::dotenv;
+use std::env;
+
+use mongodb::{options::ClientOptions, Client};
 
 #[get("/")]
 fn index() -> Template {
@@ -20,11 +26,28 @@ fn index() -> Template {
     Template::render("index", &context)
 }
 
+fn mimetype_to_format(mimetype: &str) -> image::ImageFormat {
+    match mimetype {
+        "image/png" => image::ImageFormat::Png,
+        "image/jpeg" => image::ImageFormat::Jpeg,
+        "image/gif" => image::ImageFormat::Gif,
+        "image/webp" => image::ImageFormat::WebP,
+        "image/pnm" => image::ImageFormat::Pnm,
+        "image/tiff" => image::ImageFormat::Tiff,
+        "image/tga" => image::ImageFormat::Tga,
+        "image/dds" => image::ImageFormat::Dds,
+        "image/bmp" => image::ImageFormat::Bmp,
+        "image/ico" => image::ImageFormat::Ico,
+        "image/hdr" => image::ImageFormat::Hdr,
+        "image/farbfeld" => image::ImageFormat::Farbfeld,
+        "image/avif" => image::ImageFormat::Avif,
+        // idk just go with jpeg it'll probably fail
+        _ => image::ImageFormat::Jpeg,
+    }
+}
+
 #[post("/upload", data = "<data>")]
-async fn image_upload(
-    content_type: &ContentType,
-    data: Data<'_>,
-) -> Result<&'static str, io::Error> {
+async fn image_upload(content_type: &ContentType, data: Data<'_>) -> Result<&'static str, String> {
     let mut options = MultipartFormDataOptions::with_multipart_form_data_fields(vec![
         MultipartFormDataField::file("image")
             .content_type_by_string(Some(mime::IMAGE_STAR))
@@ -47,24 +70,41 @@ async fn image_upload(
         println!("content type: {:?}", _content_type);
         println!("file name: {:?}", _file_name);
         println!("path: {:?}", _path);
-        let img = ImageReader::open(_path)?.decode()?.to_rgba16();
-        let mut encoder = encoder_builder().build()?;
-        let buffer: EncoderResult<f32> = encoder.encode(&img, img.width(), img.height())?;
+        let mut read_image = match ImageReader::open(_path) {
+            Ok(read_image) => read_image,
+            Err(e) => return Err(e.to_string()),
+        };
+        let mimetype_string = match _content_type {
+            Some(mimetype_string) => mimetype_string.to_string(),
+            None => return Err("No mimetype".to_string()),
+        };
+        read_image.set_format(mimetype_to_format(&mimetype_string.as_str()));
+        let decoded_image = match read_image.decode() {
+            Ok(decoded_image) => decoded_image,
+            Err(e) => return Err(e.to_string()),
+        };
+        let img = decoded_image.to_rgba8();
+        let result = kagamijxl::encode_memory(&img, img.width() as usize, img.height() as usize)?;
+        fs::write("image.jxl", result).unwrap();
 
         // You can now deal with the uploaded file.
-        "epic"
+        Ok("epic")
     } else {
-        "no image selected :("
+        Ok("no image selected :(")
     }
 }
 
 #[launch]
-fn rocket() -> _ {
+async fn rocket() -> _ {
+    dotenv().ok();
+
+    let mongodb_uri = env::var("MONGODB_URI").expect("MONGODB_URI must be set");
+    let client_options = ClientOptions::parse(mongodb_uri).await.unwrap();
+    let client = Client::with_options(client_options).unwrap();
+    let db = client.database("mydb");
+
     rocket::build()
+        .manage(client)
         .mount("/", routes![index, image_upload])
         .attach(Template::fairing())
 }
-
-// fn main() {
-//     println!("Hello, world!");
-// }
